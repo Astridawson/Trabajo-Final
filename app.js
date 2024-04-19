137346a39e5fbfd178e888966f5f031a3afb5cdd
Astrid <dawsonastrid@yahoo.es>
2024-04-18 19:16:00 -0500

    nuevas carpetas y archivost

---
+++ app.js
@@ -0,0 +1,24 @@
+//1.  IMPORTAR LAS DEPENDENCIAS QUE VAMOS A USAR
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv' ;
+
+
+
+// 2. CONFIGURAR NUESTRO SERVIDOR
+const app = express();
+const puerto = 9000 ;
+
+// 2.1 CONFIGURAR LAS VARIABLES DE ENTORNO
+dotenv.config();
+
+
+// 3.  ESTABLECER LA CONEXION CON NUESTRO FRONT
+
+
+
+// 4. INICIALIZAMOS EL SERVIDOR
+
+app.listen(puerto, () => {
+console.log (`El servidor esta escuchando en http://localhost:${puerto}`);
+});
