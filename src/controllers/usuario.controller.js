c2d4e4c36db87b034ccce76de0fd5745b7ebd785
Astrid <dawsonastrid@yahoo.es>
2024-04-26 18:57:09 -0500

    actualizacion 26-04

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -14,13 +14,30 @@ import usuarioModel from "../models/usuario.model.js";
 //LOGICA PARA MOSTRAR USUARIOS
 
 export const getUsuario = async(req, res) => {
-    return res.send("funciona la peticion GET");
+    //manejo de errores con try y catch
+    try{
+    let usuarios = await usuarioModel.find();
+    return res.send(usuarios);
+    }catch(error){
+        return res.json({error: "error al mostrar los datos" + error});
+    }
 
 }
 
-//LOGICA PARA CREAR USUARIOS
-export const postUsuario = async(req, res) =>{
-    return res.send("funciona la peticion POST");
+//LÓGICA PARA CREAR USUARIOS
+export const postUsuario = async(req, res) => {
+    //manejo de errores con try y catch
+
+    try{
+        let datosUsuario = req.body;
+        let nuevoUsuario = await usuarioModel.create(datosUsuario);
+        return res.json(nuevoUsuario);
+
+    }catch(error){
+        //Para que me muestre un mensaje que me indique cuál es el error
+        return res.json({error: "error al crear el usuario, holi", message:error.message});
+    }
+
 }
 
 //LOGICA PARA MODIFICAR USUARIOS
